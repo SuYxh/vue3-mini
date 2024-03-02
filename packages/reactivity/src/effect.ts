acc81d74af74e2589abdb920104a8098aa82e370
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-03-02 21:55:37 +0800

    feat: 构建 Dep模块

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,4 +1,7 @@
-type KeyToDepMap = Map<any, ReactiveEffect>
+import { isArray } from '@vue/shared'
+import { Dep, createDep } from './dep'
+
+type KeyToDepMap = Map<any, Dep>
 
 /**
  * 单例，当前的 effect
@@ -18,7 +21,7 @@ const targetMap = new WeakMap<any, KeyToDepMap>()
  * 响应性触发依赖时的执行类
  */
 export class ReactiveEffect<T = any> {
-	constructor(public fn: () => T) {}
+	constructor(public fn: () => T) { }
 
 	run() {
 		// 为 activeEffect 赋值
@@ -48,31 +51,74 @@ export function effect<T = any>(fn: () => T) {
  * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
  */
 export function track(target: object, key: unknown) {
-	// 如果当前不存在执行函数，则直接 return
-	if (!activeEffect) return
-	// 尝试从 targetMap 中，根据 target 获取 map
-	let depsMap = targetMap.get(target)
-	// 如果获取到的 map 不存在，则生成新的 map 对象，并把该对象赋值给对应的 value
-	if (!depsMap) {
-		targetMap.set(target, (depsMap = new Map()))
-	}
-	//为指定 map，指定key 设置回调函数
-	depsMap.set(key, activeEffect)
-  // 临时打印
-	console.log(targetMap)
+  // 如果当前不存在执行函数，则直接 return
+  if (!activeEffect) return
+  // 尝试从 targetMap 中，根据 target 获取 map
+  let depsMap = targetMap.get(target)
+  // 如果获取到的 map 不存在，则生成新的 map 对象，并把该对象赋值给对应的 value
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  // 获取指定 key 的 dep
+  let dep = depsMap.get(key)
+  // 如果 dep 不存在，则生成一个新的 dep，并放入到 depsMap 中
+  if (!dep) {
+    depsMap.set(key, (dep = createDep()))
+  }
+
+  trackEffects(dep)
 }
+
+/**
+ * 利用 dep 依次跟踪指定 key 的所有 effect
+ * @param dep
+ */
+export function trackEffects(dep: Dep) {
+	dep.add(activeEffect!)
+}
+
 
 /**
  * 触发依赖的方法
  * @param target WeakMap 的 key
  * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
- * @param newValue 指定 key 的最新值
- * @param oldValue 指定 key 的旧值
  */
 export function trigger(
 	target: object,
 	key?: unknown,
-	newValue?: unknown
 ) {
-	console.log('trigger: 触发依赖')
+	// 依据 target 获取存储的 map 实例
+	const depsMap = targetMap.get(target)
+	// 如果 map 不存在，则直接 return
+	if (!depsMap) {
+		return
+	}
+	// 依据指定的 key，获取 dep 实例
+	let dep: Dep | undefined = depsMap.get(key)
+	// dep 不存在则直接 return
+	if (!dep) {
+		return
+	}
+	// 触发 dep
+	triggerEffects(dep)
+}
+
+
+/**
+ * 依次触发 dep 中保存的依赖
+ */
+export function triggerEffects(dep: Dep) {
+	// 把 dep 构建为一个数组
+	const effects = isArray(dep) ? dep : [...dep]
+	// 依次触发
+	for (const effect of effects) {
+		triggerEffect(effect)
+	}
+}
+
+/**
+ * 触发指定的依赖
+ */
+export function triggerEffect(effect: ReactiveEffect) {
+	effect.run()
 }
