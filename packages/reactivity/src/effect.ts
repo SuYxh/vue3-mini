26f854ce7a7a2cf6ce09781d4462648cf490c692
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-03-02 20:34:23 +0800

    feat: 实现track

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,7 +1,18 @@
+type KeyToDepMap = Map<any, ReactiveEffect>
+
 /**
  * 单例，当前的 effect
  */
 export let activeEffect: ReactiveEffect | undefined
+
+/**
+ * 收集所有依赖的 WeakMap 实例：
+ * 1. `key`：响应性对象
+ * 2. `value`：`Map` 对象
+ * 		1. `key`：响应性对象的指定属性
+ * 		2. `value`：指定对象的指定属性的 执行函数
+ */
+const targetMap = new WeakMap<any, KeyToDepMap>()
 
 /**
  * 响应性触发依赖时的执行类
@@ -30,13 +41,25 @@ export function effect<T = any>(fn: () => T) {
 	_effect.run()
 }
 
+
 /**
  * 用于收集依赖的方法
  * @param target WeakMap 的 key
  * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
  */
 export function track(target: object, key: unknown) {
-	console.log('track: 收集依赖')
+	// 如果当前不存在执行函数，则直接 return
+	if (!activeEffect) return
+	// 尝试从 targetMap 中，根据 target 获取 map
+	let depsMap = targetMap.get(target)
+	// 如果获取到的 map 不存在，则生成新的 map 对象，并把该对象赋值给对应的 value
+	if (!depsMap) {
+		targetMap.set(target, (depsMap = new Map()))
+	}
+	//为指定 map，指定key 设置回调函数
+	depsMap.set(key, activeEffect)
+  // 临时打印
+	console.log(targetMap)
 }
 
 /**
